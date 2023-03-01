a0c3e2a2d6143c9c8b9a31728610cc2260873876
mahmuda_munny <67213233+munny006@users.noreply.github.com>
2023-03-02 00:58:09 +0600

    navbar url link

--- news-portal.js
+++ news-portal.js
@@ -11,9 +11,15 @@ const showCategoriees = data =>{
     data.news_category.forEach(singleCategory =>{
         
         let linkContainer = document.createElement('p');
-        linkContainer.innerHTML =  `<a class = "nav-link" href="#">${singleCategory.category_name}</a>`;
+        linkContainer.innerHTML =  `<a class = "nav-link" href="#" onclick = "fetchAllNewses('${singleCategory.category_id}')">${singleCategory.category_name}</a>`;
         categoorieesContainer.appendChild(linkContainer)
        
 
     })
 }
+ const fetchAllNewses = category_id =>{
+    // console.log(category_id);
+    const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
+    fetch(url).then(res => res.json())
+    .then(data =>console.log(data))
+ }
