ab548c8eba41236352bc08cd5cfe2cf16bd51dfc
mahmuda_munny <67213233+munny006@users.noreply.github.com>
2023-03-02 00:36:12 +0600

    navbar api

--- news-portal.js
+++ news-portal.js
@@ -2,7 +2,18 @@ const fetchCategories = () =>{
     fetch(`https://openapi.programming-hero.com/api/news/categories`)
     .then(res =>res.json())
     .then(data=>{
-        console.log(data);
+        showCategoriees(data.data);
     })
 }
 // fetchCategories();
+const showCategoriees = data =>{
+    const categoorieesContainer = document.getElementById('categories-container');
+    data.news_category.forEach(singleCategory =>{
+        
+        let linkContainer = document.createElement('p');
+        linkContainer.innerHTML =  `<a class = "nav-link" href="#">${singleCategory.category_name}</a>`;
+        categoorieesContainer.appendChild(linkContainer)
+       
+
+    })
+}
