4380f8f6c972402141b0f18c8760cb6ed2c962dc
mahmuda_munny <67213233+munny006@users.noreply.github.com>
2023-03-02 13:25:09 +0600

    navdata collect

--- news-portal.js
+++ news-portal.js
@@ -10,16 +10,21 @@ const showCategoriees = data =>{
     const categoorieesContainer = document.getElementById('categories-container');
     data.news_category.forEach(singleCategory =>{
         
-        let linkContainer = document.createElement('p');
-        linkContainer.innerHTML =  `<a class = "nav-link" href="#" onclick = "fetchAllNewses('${singleCategory.category_id}')">${singleCategory.category_name}</a>`;
-        categoorieesContainer.appendChild(linkContainer)
+       const linkContainer = document.createElement('p');
+        linkContainer.innerHTML =  `<a class = "nav-link" href="#" onclick = "fetchAllNews('${singleCategory.category_id}','${singleCategory.category_name}')">${singleCategory.category_name}</a>`;
+        categoorieesContainer.appendChild(linkContainer);
        
 
     })
 }
- const fetchAllNewses = category_id =>{
+ const fetchAllNews = (category_id,category_name )=>{
     // console.log(category_id);
     const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
     fetch(url).then(res => res.json())
-    .then(data =>console.log(data))
+    .then(data =>showAllNews(data,category_name))
+ }
+
+ const showAllNews = (data,category_name) => {
+    console.log(data,category_name);
+    document.getElementById('news-count').innerText = data.data.length
  }
