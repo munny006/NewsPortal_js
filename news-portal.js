0911ca6ae4df147712426a4ecb55200d17f3face
mahmuda_munny <67213233+munny006@users.noreply.github.com>
2023-03-07 14:32:58 +0600

    card create

--- news-portal.js
+++ news-portal.js
@@ -29,7 +29,27 @@ const showCategoriees = data =>{
     document.getElementById('news-count').innerText = data.length
     document.getElementById('category_name').innerText = category_name;
 
+        const newsContainer = document.getElementById('all-news')
     data.forEach(singleNews =>{
-        console.log(singleNews);
+        // console.log(singleNews);
+        // newsContainer.innerHTML += ``
+        const card = document.createElement('div');
+        card.classList.add('card','mb-3');
+        card.innerHTML = `
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="..." class="img-fluid rounded-start" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        
+      </div>`;
+      newsContainer.appendChild(card)
+
     })
  }
