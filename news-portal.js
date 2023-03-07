7134cb5871227b3355f1ebe6325e4842182c4548
mahmuda_munny <67213233+munny006@users.noreply.github.com>
2023-03-07 23:15:34 +0600

    create modal

--- news-portal.js
+++ news-portal.js
@@ -48,7 +48,7 @@ const showCategoriees = data =>{
               <p class="card-text">${singleNews.details.slice(0,230)}...</p>
              
             </div>
-            <div class="card-footer border-0 bg-body">
+            <div class="card-footer border-0 bg-body d-flex justify-content-between">
             <div class="d-flex gap-2">
             <img src="${singleNews.author.img}" class="img-fluid  rounded-circle" alt="..." height="40" width="40">
             <div>
@@ -56,9 +56,21 @@ const showCategoriees = data =>{
             <p class="m-0 p-0">${singleNews.author.published_date}</p>
             </div>
             </div>
-            <div> </div>
-            <div> </div>
-            <div> </div>
+            <div class = "d-flex align-items-center">
+            <i class="fa fa-eye" aria-hidden="true"></i>
+            <p class="m-0 p-0">${singleNews.total_view}</p>
+            
+            </div>
+            <div>
+            <i class="fa fa-star-o" aria-hidden="true" style="margin-top: 15px;"></i>
+            <i class="fa fa-star-o" aria-hidden="true" style="margin-top: 15px;"></i>
+            <i class="fa fa-star-o" aria-hidden="true" style="margin-top: 15px;"></i>
+            <i class="fa fa-star-o" aria-hidden="true" style="margin-top: 15px;"></i>
+            </div>
+            <div> 
+            <i class="fa fa-arrow-circle-o-right" aria-hidden="true" style="font-size: 22px;margin-top: 11px;
+            color: blue;" onclick="fetchNewsDetails('${singleNews.news_id}')" data-bs-toggle="modal" data-bs-target="#exampleModal"></i>
+            </div>
             </div>
           </div>
         
@@ -66,4 +78,13 @@ const showCategoriees = data =>{
       newsContainer.appendChild(card)
 
     })
+ }
+ const fetchNewsDetails = news_id=>{
+  let url = `https://openapi.programming-hero.com/api/news/${news_id}`
+ fetch(url).then(res=>res.json())
+ .then(data=>showNewsDetail(data.data[0]))
+
+ }
+ const showNewsDetail = newsDetail =>{
+
  }
