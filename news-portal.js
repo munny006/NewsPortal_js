b1fb79b5f73092bd3f6c20c24e43b9a1bd50cf41
mahmuda_munny <67213233+munny006@users.noreply.github.com>
2023-03-07 14:43:06 +0600

    title details done

--- news-portal.js
+++ news-portal.js
@@ -44,8 +44,8 @@ const showCategoriees = data =>{
           </div>
           <div class="col-md-8">
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <h5 class="card-title">${singleNews.title}</h5>
+              <p class="card-text">${singleNews.details.slice(0,230)}</p>
               <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
             </div>
           </div>
